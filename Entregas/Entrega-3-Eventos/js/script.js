176e49c09c828ff45523ee3af9c56b3a0ad59030
Federico Lopez <lopezfedericojavier1@gmail.com>
2022-07-29 18:51:22 -0300

    Entrega Eventos

--- Entregas/Entrega-3-Eventos/js/script.js
+++ Entregas/Entrega-3-Eventos/js/script.js
@@ -1,43 +1,168 @@
-let bodyy = document.querySelector("body");
-let btnLight = document.getElementById("light");
-let btnDark = document.getElementById("dark");
+//Cambio de titulo y subtitulo
+const titulo = document.getElementById('titulo');
 
-btnLight.onclick = () => {
-    console.log("SUNLIIGHT!!");
-    bodyy.className = "modLight";
+titulo.innerText = 'LO MEJOR DE LA TECNOLOGIA';
+
+const subtitulo = document.getElementById('subtitulo');
+
+subtitulo.innerText = 'Lo  vas a encontrar acá!!';
+
+
+//Creando los elementos HTML
+
+class Celular {
+    constructor(celular) {
+        this.id = celular.id;
+        this.marca = celular.marca;
+        this.memoriaInterna = celular.memoriaInterna;
+        this.precio = celular.precio;
+        this.condicion = celular.condicion;
+    }
 };
 
-btnDark.onclick = () => {
-    console.log("NIGHTCALL!!!");
-    bodyy.className = "modDark";
+const celulares = [{
+        id: 0,
+        marca: 'SAMSUNG A12',
+        memoriaInterna: '64gb Memoria Interna',
+        precio: 73999,
+        imagen: './images/samsunga12.jpg',
+        condicion: 'Condicion: NUEVO'
+    },
+    {
+        id: 1,
+        marca: 'SAMSUNG A53',
+        memoriaInterna: '128gb Memoria Interna',
+        precio: 108999,
+        imagen: './images/samsunga53.png',
+        condicion: 'Condicion: USADO'
+    },
+    {
+        id: 2,
+        marca: 'SAMSUNG NOTE 10',
+        memoriaInterna: '256gb Memoria Interna',
+        precio: 129999,
+        imagen: './images/samsungnote10.jpg',
+        condicion: 'Condicion: USADO'
+    },
+    {
+        id: 3,
+        marca: 'SAMSUNG S22 ULTRA',
+        memoriaInterna: '256gb Memoria Interna',
+        precio: 339999,
+        imagen: './images/samsungs22.jpg',
+        condicion: 'Condicion: NUEVO'
+    },
+    {
+        id: 4,
+        marca: 'IPAD PRO 12.9"',
+        memoriaInterna: '256gb Memoria Interna',
+        precio: 209999,
+        imagen: './images/ipadpro129.jpg',
+        condicion: 'Condicion: NUEVO'
+    },
+    {
+        id: 5,
+        marca: 'IPAD PRO 11',
+        memoriaInterna: '128gb Memoria Interna',
+        precio: 300000,
+        imagen: './images/ipadpro11.jpg',
+        condicion: 'Condicion: NUEVO'
+    },
+    {
+        id: 6,
+        marca: 'IPAD MINI',
+        memoriaInterna: '64gb Memoria Interna',
+        precio: 149999,
+        imagen: './images/ipadmini64.jpg',
+        condicion: 'Condicion: NUEVO'
+    },
+    {
+        id: 7,
+        marca: 'AIRPODS PRO',
+        memoriaInterna: '256gb Memoria Interna',
+        precio: 59999,
+        imagen: './images/airpodspro.jpg',
+        condicion: 'Condicion: NUEVO'
+    },
+    {
+        id: 8,
+        marca: 'IPHONE XS',
+        memoriaInterna: '64gb Memoria Interna',
+        precio: 178000,
+        imagen: './images/iphonexs.png',
+        condicion: 'Condicion: NUEVO'
+    },
+    {
+        id: 9,
+        marca: 'IPHONE 11 PRO',
+        memoriaInterna: '128gb Memoria Interna',
+        precio: 287000,
+        imagen: './images/iphone11pro.jpg',
+        condicion: 'Condicion: NUEVO'
+    },
+    {
+        id: 10,
+        marca: 'IPHONE 13 PRO',
+        memoriaInterna: '256gb Memoria Interna',
+        precio: 565000,
+        imagen: './images/iphone13pro.jpg',
+        condicion: 'Condicion: NUEVO'
+    },
+    {
+        id: 11,
+        marca: 'IPHONE 8 PLUS',
+        memoriaInterna: '256gb Memoria Interna',
+        precio: 230000,
+        imagen: './images/iphone8plus.png',
+        condicion: 'Condicion: NUEVO'
+    }
+];
+
+const cards = document.getElementById('contenedor');
+
+//invocando las funciones
+crearCards();
+
+function crearCards() {
+    for (const celular of celulares) {
+        cards.innerHTML += `
+        <div class="col mt-4 text-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="${celular.imagen}" class="card-img-top" alt="...">
+                    <div class="card-body border-2">
+                        <h5 class="card-title text-decoration-underline text-center">${celular.marca}</h5>
+                        <p class="card-text text-center">
+                            <ul id="ul">
+                                <li>${celular.memoriaInterna}</li>
+                                <li>${celular.condicion}</li>
+                                <li class="precio">$${celular.precio}</li>
+                            </ul>
+                        </p>
+                        <button class="btn btn-primary" type="submit" id='boton${celular.id}'>Agregar al carrito</button>
+                    </div>
+                </div>
+            </div>`
+    }
 };
 
-let contenedor = document.getElementById("contenedor");
-const count = document.createElement("h1");
-count.innerText = "CONTADOR";
-contenedor.appendChild(count);
-const btn1 = document.createElement("button");
-btn1.innerText = "SUBE";
-btn1.className = "btn btn-info";
-contenedor.appendChild(btn1);
-const btn2 = document.createElement("button");
-btn2.innerText = "BAJA";
-btn2.className = "btn btn-danger";
-contenedor.appendChild(btn2);
-const contador = document.createElement("h4");
-contador.innerHTML = "Contador: <span>0</span>";
-contenedor.appendChild(contador);
-contador.className = "my-5";
+//CREANDO EVENTOS
+let carrito = [];
 
-const span = document.querySelector("span");
-let cuenta = 0;
-btn1.addEventListener("click", () => {
-    console.log("ESTAS SUBIENDOOO!");
-    cuenta++;
-    span.innerText = cuenta;
+//Asigna un evento a cada boton
+celulares.forEach(celular => {
+    document.getElementById(`boton${celular.id}`).addEventListener('click', function () {
+        agregarCarrito(celular);
+    });
 });
-btn2.addEventListener("click", () => {
-    console.log("ESTAS BAJANDOOO!!!");
-    cuenta--;
-    span.innerText = cuenta;
-});
+
+
+//Agrega al carrito creando una tabla
+function agregarCarrito(celularComprado) {
+    document.getElementById('tableBody').innerHTML += `
+    <tr>
+    <td>${celularComprado.id}</td>
+    <td>${celularComprado.marca}</td>
+    <td>${celularComprado.memoriaInterna}</td>
+    <td>$ ${celularComprado.precio}</td>
+    </tr>`
+};
